92b682dbc7c796e70997a2c9c1c2dd2df3ec982c
anjalmaharjan <heromaharjan@gmail.com>
2025-01-08 17:40:00 +0545

    new changes

--- lib/src/hls/hls_parser/variant_info.dart
+++ lib/src/hls/hls_parser/variant_info.dart
@@ -42,5 +42,10 @@ class VariantInfo {
 
   @override
   int get hashCode => Object.hash(
-      bitrate, videoGroupId, audioGroupId, subtitleGroupId, captionGroupId);
+        bitrate,
+        videoGroupId,
+        audioGroupId,
+        subtitleGroupId,
+        captionGroupId,
+      );
 }
